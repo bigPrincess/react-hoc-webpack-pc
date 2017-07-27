045d07d86cc201019ac7d48f820b6230909f611c
danyunLu <ldy0700@163.com>
2017-07-27 18:29:51 +0800

    upload

--- src/components/cropperimg/components/cropperimg.tsx
+++ src/components/cropperimg/components/cropperimg.tsx
@@ -1,19 +1,69 @@
 import * as  React from 'react';
 import { IProps } from '../constants/cropperimg';
 import { hoc } from '../containers/cropperimg';
+import { Upload, Button, Icon } from 'antd';
 
 import '../styles/cropperimg.scss';
+
 
 export class CropperImgComponent extends React.Component<IProps, any>{
     constructor(props, content) {
         super(props, content);
     }
+    state = {
+        fileList: [{
+            uid: -1,
+            name: 'xxx.png',
+            status: 'done',
+            url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
+        }],
+    }
+    handleChange = (info) => {
+        let fileList = info.fileList;
+
+        // 1. Limit the number of uploaded files
+        //    Only to show two recent uploaded files, and old ones will be replaced by the new
+        fileList = fileList.slice(-4);
+
+        // 2. read from response and show file link
+        fileList = fileList.map((file) => {
+            if (file.response) {
+                // Component will show file.url as link
+                file.url = file.response.url;
+            }
+            return file;
+        });
+
+        // 3. filter successfully uploaded files according to response from server
+        fileList = fileList.filter((file) => {
+            if (file.response) {
+                return file.response.status === 'success';
+            }
+            return true;
+        });
+
+        this.setState({ fileList });
+    }
     render(): JSX.Element {
+        const props = {
+            action: '//jsonplaceholder.typicode.com/posts/',
+            onChange: this.handleChange,
+            multiple: true,
+            listType: 'picture',
+            className: 'upload-list-inline',
+        };
+        console.log(this.state.fileList)
         return (
             <div>
-                666
+                <Upload {...props} 
+                    fileList={this.state.fileList}
+                    accept="image">
+                    <Button>
+                        <Icon type="upload" /> upload
+                    </Button>
+                </Upload>
             </div>
-            
+
         );
     }
 }
