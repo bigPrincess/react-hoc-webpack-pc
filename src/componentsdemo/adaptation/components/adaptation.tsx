be457b33986a2b2428ed8e9e07eefa522369394e
qinwen <ailsa.qin@chinaredstar.com>
2017-07-27 14:12:35 +0800

    自适应提交

---
+++ src/componentsdemo/adaptation/components/adaptation.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+
+import { hoc } from '../containers/adaptation';
+import { IProps } from '../constants/adaptation';
+import { Row, Col } from 'antd';
+
+import '../styles/adaptation.scss';
+
+export class ApdaComponent extends React.Component<IProps, any> {
+    render() {
+        return (
+            <div className="adaptation-content">
+                <Row>
+                    <Col xs={12} sm={12} md={6} lg={6} xl={4}>Col</Col>
+                    <Col xs={12} sm={12} md={6} lg={6} xl={4}>Col</Col>
+                    <Col xs={12} sm={12} md={6} lg={6} xl={4}>Col</Col>
+                    <Col xs={12} sm={12} md={6} lg={6} xl={4}>Col</Col>
+                </Row>
+            </div>
+        );
+    }
+}
+
+export const ApdaComponentHoc = hoc(ApdaComponent);
--- src/componentsdemo/home/components/home.tsx
+++ src/componentsdemo/home/components/home.tsx
@@ -36,17 +36,16 @@ export class HomeComponent extends React.Component<IProps, any> {
                     {
                         "name": "图片上传、剪切、下载",
                         "url": "cropperimg"
+                    },
+                    {
+                        "name":"自适应",
+                        "url":"ApdaComponent"
                     }
                 ]
             }
         ]
         return (
             <div className="demo-list">
-                {/* <div><Link to="/components/http">http请求 ⇨</Link></div>
-                <div><Link to="/components/antd">antd ⇨</Link></div>
-                <div><Link to="/components/imgzoomin">图片点击放大 ⇨</Link></div>
-                <div><Link to="/components/icon">icon图标 ⇨</Link></div>
-                <div><Link to="/components/cropperimg">图片上传、剪切、下载 ⇨</Link></div>                   */}
                 <AccordionComponent width={'30'} height={'30'} menuArrs={accordarr} eveType = {'click'} currentColor={'rgba(102,102,102,1)'} title="这个是插件DEMO页哦"/>
            </div>
         );
