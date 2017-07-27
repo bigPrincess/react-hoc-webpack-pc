bf9a498a425b2278b0c0c8f8ada133623f87724e
danyunLu <ldy0700@163.com>
2017-07-27 17:25:05 +0800

    删除header相关文件

--- src/componentsdemo/imgzoomin/constants/imgzoomin.tsx
+++ src/componentsdemo/imgzoomin/constants/imgzoomin.tsx
@@ -1,9 +1,5 @@
-import { IHeaderColor } from "../../../components/header/hoc/header";
 
-export interface IProps extends IHeaderColor {
-    //滑动距离
-    sTop:number;
-    setScrollTop:Function;
-    handleScroll?:Function;
+export interface IProps {
+    
 
 }
--- src/componentsdemo/imgzoomin/containers/imgzoomin.tsx
+++ src/componentsdemo/imgzoomin/containers/imgzoomin.tsx
@@ -1,5 +1,5 @@
 import { compose} from "recompose";
-import { IProps } from '../constants/headerapi';
+import { IProps } from '../constants/imgzoomin';
 
 export const hoc = compose<IProps, any>(
 );
