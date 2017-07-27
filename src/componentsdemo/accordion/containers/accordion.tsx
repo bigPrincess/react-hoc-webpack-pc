7b03b313e4370318f3f3e19a935c63c875084c9f
qinwen <ailsa.qin@chinaredstar.com>
2017-07-27 10:40:04 +0800

    删除无用的header

--- src/componentsdemo/accordion/containers/accordion.tsx
+++ src/componentsdemo/accordion/containers/accordion.tsx
@@ -1,7 +1,5 @@
 import { compose} from "recompose";
 import { IProps } from '../constants/accordion';
-import { getHeaderHoc } from "../../../components/header/index";
 
 export const hoc = compose<IProps, any>(
-    getHeaderHoc(),
 );
