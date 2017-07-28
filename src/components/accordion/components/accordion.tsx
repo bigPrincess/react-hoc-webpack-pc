4b1001d68441678cf56808e160ecce5246ad6fa6
SWFDE-LIQING <1186963387@qq.com>
2017-07-28 14:54:39 +0800

    代码整合优化~

--- src/components/accordion/components/accordion.tsx
+++ src/components/accordion/components/accordion.tsx
@@ -1,7 +1,7 @@
 import * as  React from 'react';
 import { IProps } from '../constants/accordion';
 import { hoc } from '../containers/accordion';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import {IconComments} from '../../icons';
 
 
@@ -24,11 +24,11 @@ export class AccordionComponent extends React.Component<IProps, any>{
                         return (
                                 <li>
                                     <div className="link">
-                                            <input type="hidden" className="pageUrl" value={(key.url && key.url.length >0) ? (key.url) : null}/>
-                                            <input type="hidden" className="objId" value={(key.id && key.id.length >0) ? (key.id) : null}/>
-                                            <IconComments  width={accData.width || 20} height={accData.height || 20} color={accData.currentColor} type={accData.lefticon}/>                                                         
-                                            <span className='menu-name'>{key.name}</span>  
-                                            <IconComments classname={(!key.submenu) ? "more" : null} width={accData.width || 16} height={accData.height || 16} color={accData.currentColor} type={accData.righticon || 'more'}/>         
+                                        <input type="hidden" className="pageUrl" value={(key.url && key.url.length >0) ? (key.url) : null}/>
+                                        <input type="hidden" className="objId" value={(key.id && key.id.length >0) ? (key.id) : null}/>
+                                        <IconComments  width={accData.width || 20} height={accData.height || 20} color={accData.currentColor} type={accData.lefticon}/>                                                         
+                                        <span className='menu-name'>{key.name}</span>  
+                                        <IconComments classname={(!key.submenu) ? "more" : null} width={accData.width || 16} height={accData.height || 16} color={accData.currentColor} type={accData.righticon || 'more'}/>         
                                     </div>
                                     {(key.submenu && key.submenu.length > 0) ? own.renderCreateSubMenu(key.submenu,accData) : null}
                                 </li>
@@ -51,11 +51,11 @@ export class AccordionComponent extends React.Component<IProps, any>{
                         return (
                             <li>
                                 <div className="link">
-                                        <input type="hidden" className="pageUrl" value={(key.url && key.url.length > 0) ? (key.url) : null}/>
-                                        <input type="hidden" className="objId" value={(key.id && key.id.length > 0) ? (key.id) : null}/>
-                                        <IconComments  width={accData.width || 20} height={accData.height || 20} color={accData.currentColor} type={accData.lefticon}/>                                                         
-                                        <span className='menu-name'>{key.name}</span>  
-                                        <IconComments classname={(!key.submenu) ? "more" : null}  width={accData.width || 16} height={accData.height || 16} color={accData.currentColor} type={accData.righticon || 'more'}/>         
+                                    <input type="hidden" className="pageUrl" value={(key.url && key.url.length > 0) ? (key.url) : null}/>
+                                    <input type="hidden" className="objId" value={(key.id && key.id.length > 0) ? (key.id) : null}/>
+                                    <IconComments  width={accData.width || 20} height={accData.height || 20} color={accData.currentColor} type={accData.lefticon}/>                                                         
+                                    <span className='menu-name'>{key.name}</span>  
+                                    <IconComments classname={(!key.submenu) ? "more" : null}  width={accData.width || 16} height={accData.height || 16} color={accData.currentColor} type={accData.righticon || 'more'}/>         
                                 </div>
                                 {(key.submenu && key.submenu.length > 0) ? (own.renderCreateSubMenu(key.submenu,accData)) : null}
                             </li>
